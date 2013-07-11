1a392381203cac93d7a046a57bb1254d5be231d4
Joe Forsythe <j.forsythe@modcloth.com>
2013-07-11 14:21:53 -0400

    Added ssl capabilities

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -2,12 +2,23 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
+import org.eclipse.jetty.util.ssl.SslContextFactory
+import org.eclipse.jetty.server.ssl.SslSocketConnector
 
 object JettyLauncher {
   def main(args: Array[String]) {
     val port = (Option[String](System.getenv("PORT")).getOrElse("8080")).toInt
 
     val server = new Server(port)
+    val factory = new SslContextFactory(System.getProperty("user.dir") + "/temp/keystore")
+    factory.setKeyStorePassword("modcloth")
+    factory.setTrustStore(System.getProperty("user.dir") + "/temp/keystore")
+    factory.setTrustStorePassword("modcloth")
+
+    val connector = new SslSocketConnector(factory)
+    connector.setPort(8443)
+
+    server.setConnectors(Array(connector))
     val context = new WebAppContext()
     context setContextPath "/"
     context.setResourceBase("src/main/webapp")
--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -7,5 +7,7 @@ import com.modcloth.druidia._
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.mount(new LoggingServlet, "/*")
+
+    context.setInitParameter(ScalatraBase.ForceHttpsKey, "true")
   }
 }
